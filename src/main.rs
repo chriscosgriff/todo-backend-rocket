59c4af2875f563b0e2c8aa31c87dd1c646a0e727
Chris Cosgriff <chris.cosgriff@gmail.com>
2017-02-23 16:33:34 +0000

    Add CORS responder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(plugin)]
+#![feature(specialization)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
@@ -11,10 +12,27 @@ extern crate serde_derive;
 use rocket_contrib::JSON;
 use rocket::config::{Config, ConfigError, Environment};
 use std::env;
+use rocket::response::{Responder, Response};
+use rocket::http::Status;
 
 #[derive(Serialize)]
 struct Todo {
     title: String,
+}
+
+struct CORS<R>(R);
+
+impl<'r, R: Responder<'r>> Responder<'r> for CORS<R> {
+    default fn respond(self) -> Result<Response<'r>, Status> {
+        let mut build = Response::build();
+        build.merge(self.0.respond()?);
+
+        build.raw_header("Access-Control-Allow-Origin", "*")
+            .raw_header("Access-Control-Allow-Methods",
+                        "GET, PUT, PATCH, POST, OPTIONS")
+            .raw_header("Access-Control-Allow-Headers", "Content-Type")
+            .ok()
+    }
 }
 
 fn get_server_port() -> u16 {
@@ -35,8 +53,8 @@ fn start_server() -> Result<(), ConfigError> {
 }
 
 #[get("/")]
-fn index() -> Option<JSON<Todo>> {
-    Some(JSON(Todo { title: "Rocket!".to_string() }))
+fn index() -> CORS<JSON<Todo>> {
+    CORS(JSON(Todo { title: "Rocket!".to_string() }))
 }
 
 fn main() {
