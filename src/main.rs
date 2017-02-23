385d36fe26526f690c81681371a93d9d2315c6ff
Chris Cosgriff <chris.cosgriff@gmail.com>
2017-02-23 14:27:36 +0000

    Refactoring...

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@
 
 extern crate rocket;
 
-use rocket::config::{Config, Environment};
+use rocket::config::{Config, ConfigError, Environment};
 use std::env;
 
 fn get_server_port() -> u16 {
     let port = env::var("PORT").unwrap_or(String::new());
     port.parse().unwrap_or(8000)
+}
+
+fn get_config() -> Result<Config, ConfigError> {
+    Config::build(Environment::active()?)
+        .port(get_server_port())
+        .finalize()
+}
+
+fn takeoff() -> Result<(), ConfigError> {
+    Ok(rocket::custom(get_config()?, true)
+        .mount("/", routes![index])
+        .launch())
 }
 
 #[get("/")]
@@ -17,9 +29,5 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    let config = Config::build(Environment::active().unwrap())
-        .port(get_server_port())
-        .finalize()
-        .unwrap();
-    rocket::custom(config, true).mount("/", routes![index]).launch();
+    takeoff().unwrap();
 }
