2dd8841147621444a6f38902b48ca5970f658b73
Chris Cosgriff <chris.cosgriff@gmail.com>
2017-02-23 14:02:06 +0000

    Updating some settings for operating on Heroku

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,23 @@
 
 extern crate rocket;
 
+use rocket::config::{Config, Environment};
+use std::env;
+
+fn get_server_port() -> u16 {
+    let port = env::var("PORT").unwrap_or(String::new());
+    port.parse().unwrap_or(8000)
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Rocket"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    let config = Config::build(Environment::active().unwrap())
+        .port(get_server_port())
+        .finalize()
+        .unwrap();
+    rocket::custom(config, true).mount("/", routes![index]).launch();
 }
