b0548b238785035ec588f6fdb363eb1fdac7eccd
Chris Cosgriff <chris.cosgriff@gmail.com>
2017-03-03 22:17:35 +0000

    Completed todo-backend implementation

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,42 @@ extern crate rocket_contrib;
 
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
 
 use rocket_contrib::JSON;
 use rocket::config::{Config, ConfigError, Environment};
-use std::env;
 use rocket::response::{Responder, Response};
 use rocket::http::Status;
+use rocket::State;
+use std::env;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::collections::hash_map::Entry;
 
-#[derive(Serialize, Deserialize)]
+struct CurrentId(AtomicUsize);
+
+#[derive(Default, Clone, Debug, Serialize, Deserialize)]
 struct Todo {
-    title: String,
+    title: Option<String>,
+    completed: Option<bool>,
+    url: Option<String>,
+    order: Option<usize>,
+}
+
+impl Todo {
+    pub fn merge(&mut self, other: Todo) {
+        if let Some(title) = other.title {
+            self.title = Some(title);
+        }
+        if let Some(completed) = other.completed {
+            self.completed = Some(completed);
+        }
+        if let Some(order) = other.order {
+            self.order = Some(order);
+        }
+    }
 }
 
 struct CORS<R>(Option<R>);
@@ -25,16 +51,24 @@ struct CORS<R>(Option<R>);
 impl<'r, R: Responder<'r>> Responder<'r> for CORS<R> {
     default fn respond(self) -> Result<Response<'r>, Status> {
         let mut build = Response::build();
-        if let Some(inner_responder) = self.0 {
-            build.merge(inner_responder.respond()?);
+        if let Some(inner_res) = self.0 {
+            build.merge(inner_res.respond()?);
         }
 
         build.raw_header("access-control-allow-origin", "*")
             .raw_header("access-control-Allow-Methods",
-                        "HEAD, GET, PUT, PATCH, POST, OPTIONS")
+                        "OPTIONS, GET, POST, PATCH, DELETE")
             .raw_header("access-control-allow-headers", "Content-Type")
             .ok()
     }
+}
+
+lazy_static! {
+  static ref DB: Mutex<HashMap<usize, Todo>> = Mutex::new(HashMap::new());
+}
+
+fn main() {
+    start_server().unwrap();
 }
 
 fn get_server_port() -> u16 {
@@ -50,30 +84,80 @@ fn get_config() -> Result<Config, ConfigError> {
 
 fn start_server() -> Result<(), ConfigError> {
     Ok(rocket::custom(get_config()?, true)
-        .mount("/", routes![pre_flight_check, get_todo, post_todo, delete_todo])
+        .mount("/",
+               routes![cors, cors_id, list, delete_all, create, read, update, delete])
+        .manage(CurrentId(AtomicUsize::new(1)))
         .launch())
 }
 
-#[get("/")]
-fn get_todo() -> CORS<JSON<Vec<Todo>>> {
-    CORS(Some(JSON(vec![])))
+fn to_new_todo(id: usize, new_todo: Todo) -> Todo {
+    Todo {
+        title: new_todo.title.or(None),
+        completed: new_todo.completed.or(Some(false)),
+        url: Some(format!("http://localhost:8000/{}", id)),
+        order: new_todo.order.or(None),
+    }
 }
 
 #[post("/", format = "application/json", data = "<todo>")]
-fn post_todo(todo: JSON<Todo>) -> CORS<JSON<Todo>> {
-    CORS(Some(JSON(todo.into_inner())))
+fn create(todo: JSON<Todo>, current_id: State<CurrentId>) -> CORS<JSON<Todo>> {
+    let id = current_id.0.fetch_add(1, Ordering::Relaxed);
+    let mut db = DB.lock().unwrap();
+    let td = db.entry(id).or_insert(to_new_todo(id, todo.into_inner()));
+    println!("{:?}", td);
+    CORS(Some(JSON(td.clone())))
+}
+
+#[patch("/<id>", format = "application/json", data = "<todo_patched>")]
+fn update(id: usize, todo_patched: JSON<Todo>) -> CORS<JSON<Todo>> {
+    let mut db = DB.lock().unwrap();
+    if let Entry::Occupied(mut o) = db.entry(id) {
+        let mut todo = o.get_mut();
+        todo.merge(todo_patched.into_inner());
+        CORS(Some(JSON(todo.clone())))
+    } else {
+        CORS(None)
+    }
+}
+
+#[get("/<id>")]
+fn read(id: usize) -> CORS<JSON<Todo>> {
+    let db = DB.lock().unwrap();
+    // handle not found...
+    let todo = db.get(&id).unwrap();
+    CORS(Some(JSON(todo.clone())))
+}
+
+#[get("/")]
+fn list() -> CORS<JSON<Vec<Todo>>> {
+    let db = DB.lock()
+        .unwrap();
+    let todos = db.values()
+        .cloned()
+        .collect();
+    CORS(Some(JSON(todos)))
 }
 
 #[delete("/" )]
-fn delete_todo() -> CORS<()> {
-  CORS(None)
-}
-
-#[options("/")]
-fn pre_flight_check() -> CORS<()> {
+fn delete_all() -> CORS<()> {
+    let mut db = DB.lock().unwrap();
+    db.clear();
     CORS(None)
 }
 
-fn main() {
-    start_server().unwrap();
+#[delete("/<id>" )]
+fn delete(id: usize) -> CORS<()> {
+    let mut db = DB.lock().unwrap();
+    db.remove(&id);
+    CORS(None)
+}
+
+#[options("/")]
+fn cors() -> CORS<()> {
+    CORS(None)
+}
+
+#[options("/<id>")]
+fn cors_id(id: usize) -> CORS<()> {
+    CORS(None)
 }
